b6a4153796e84e1f6fb149e4e42e8a03867251e1
Isaac Sauri <isaacsauri@digitaldevops.com.mx>
2024-09-18 23:02:59 -0600

    nextpage fix

--- gapsy-test/app/page.js
+++ gapsy-test/app/page.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import Loading from "./ui/Loading";
 
 export default function Home() {
-  const [data, setData] = useState({})
+  const [data, setData] = useState([])
   const [loading, setLoading] = useState(true)
   const [keyword, setKeyword] = useState("computer")
   const [pageNumber, setPageNumber] = useState(1)
@@ -38,31 +38,34 @@ export default function Home() {
   }
 
 
-  async function fetchNexPage() {
+  async function fetchNextPage() {
     const url = `https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-search-by-keyword?keyword=${keyword}&page=${pageNumber}&sortBy=best_match`;
   
     const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': 'fce0e15738msh6a87c0c9db9505cp14b74fjsn54bc768f3bc7',
-            'x-rapidapi-host': 'axesso-walmart-data-service.p.rapidapi.com'
-        }
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': 'fce0e15738msh6a87c0c9db9505cp14b74fjsn54bc768f3bc7',
+        'x-rapidapi-host': 'axesso-walmart-data-service.p.rapidapi.com'
+      }
     };
   
     try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        const items = data.item.props.pageProps.initialData.searchResult?.itemStacks[0].items.filter((item) => item.price) || [];
-        setData(items); 
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      const newItems = data.item.props.pageProps.initialData.searchResult?.itemStacks[0].items.filter((item) => item.price) || [];
+  
+      // Concatena los datos nuevos con los datos existentes
+      setData((prevData) => [...prevData, ...newItems]);
     } catch (error) {
-        console.error('Error fetching data:', error);
+      console.error('Error fetching data:', error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
+  
 
   const handleSearchItem = (query) => {
     setKeyword(query)
@@ -78,7 +81,7 @@ export default function Home() {
   }, [keyword])
 
   useEffect(() => {
-    fetchNexPage()
+    fetchNextPage()
   }, [pageNumber])
   
   if (loading)  {
@@ -92,10 +95,7 @@ export default function Home() {
   return (
     <Page className="home-page">
       <Header className="header"/>
-      <ProductList data={data} className="item-list" nextPage={handleNext} onSearch={handleSearchItem} onEndReached={() => {
-    console.log("Se llegó al final del scroll");
-    // Aquí puedes cargar más datos o realizar alguna otra acción
-  }}/>
+      <ProductList data={data} className="item-list" nextPage={handleNext} onSearch={handleSearchItem} onEndReached={handleNext}/>
     </Page>
   ); 
 }
